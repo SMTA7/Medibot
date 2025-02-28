b207540b7066ca540d66976dcb962f66c7ddc011
unknown <thameemansarivrn@gmail.com>
2025-02-28 13:01:22 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Generative AI  project',
+    version= '0.0.0',
+    author= 'Mohamed Thameem Ansari',
+    author_email='thameemansarivrn@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
